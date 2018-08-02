0704a5dd14168bd81f52d1b8b2009f377a74141d
skyran1278 <skyran1278@gmail.com>
2018-08-02 19:58:47 +0800

    rcscan test

--- 20170524_RCScan/Test.vb
+++ 20170524_RCScan/Test.vb
@@ -1,41 +1,16 @@
 Sub TestSortRawData()
 '
-' TODO:處理筏基版厚、上下版厚
 '
-
-    Dim ran As New UTILS_CLASS
-    Dim Beam As New BeamClass
-    ' Set Beam = New BeamClass
-
-    Call ran.ExecutionTime(True)
-    Call ran.PerformanceVBA(True)
 
     On Error GoTo ErrorHandler
 
-    Call Beam.Initialize("SortRawData")
+    Dim Beam As New BeamClass
 
-    ' 實作規範
-    Beam.Norm4_9_3
-    Beam.Norm4_9_4
-    Beam.EconomicNorm4_9_4
-    Beam.SafetyRebarRatioAndSpace
-    Beam.SafetyRebarRatioForGB
-    Beam.EconomicBotRebarRelativeForGB
-    Beam.EconomicTopRebarRelativeForGB
-    Beam.SafetyStirrupSpace
-
-    Beam.PrintResult
-
-    ' Beam.CountRebarNumber
-    ' Beam.PrintRebarRatio
-
-    Call ran.PerformanceVBA(False)
-    Call ran.ExecutionTime(False)
+    Call Beam.Initialize("列數不合")
 
     Exit Sub
 
 ErrorHandler:
-    Call ran.PerformanceVBA(False)
-    Call Beam.PrintError(Err.NUMBER, Err.Source, Err.Description)
+    Call Beam.PrintError
 
 End Sub
