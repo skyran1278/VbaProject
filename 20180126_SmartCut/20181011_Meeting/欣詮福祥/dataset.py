52f5c68083ce2b8495603faf1abb5b46d61f28b1
skyran1278 <skyran1278@gmail.com>
2018-10-08 18:19:27 +0800

    smartcut data beam design

--- 20180126_SmartCut/20181011_Meeting/欣詮福祥/dataset.py
+++ 20180126_SmartCut/20181011_Meeting/欣詮福祥/dataset.py
@@ -3,27 +3,20 @@ import pickle
 import numpy as np
 
 dataset_dir = os.path.dirname(os.path.abspath(__file__))
-save_file = dataset_dir + "/moment_diagram.pkl"
+save_file = dataset_dir + "/beam_design.pkl"
 
 
-def _load_file(file_name, usecols=None):
+def _load_file(file_name):
     file_path = dataset_dir + "/" + file_name
-    beam_forces = np.genfromtxt(
-        file_path, dtype=None, names=True, delimiter='\t', encoding='utf8', usecols=usecols)
-    # beam_forces_tuple2array = np.array(beam_forces)
+    dataset = np.genfromtxt(
+        file_path, dtype=None, names=True, delimiter='\t', encoding='utf8')
 
-    return beam_forces
+    return dataset
 
 
-def init_moment():
-    dataset = {}
-
-    beam_forces = _load_file('Beam Forces.txt')
-    frame_assignments_summary = _load_file(
-        'Frame Assignments Summary.txt', usecols=(0, 1, 2, 3))
-
-    dataset['beam_force'] = beam_forces
-    dataset['beam_length'] = frame_assignments_summary
+def init_pkl():
+    dataset = _load_file(
+        'Concrete Design 2 Beam Summary Data ACI 318-05 IBC 2003.txt')
 
     print("Creating pickle file ...")
     with open(save_file, 'wb') as f:
@@ -31,21 +24,20 @@ def init_moment():
     print("Done!")
 
 
-def load_moment():
+def load_beam_design():
     if not os.path.exists(save_file):
-        init_moment()
+        init_pkl()
 
     with open(save_file, 'rb') as f:
         dataset = pickle.load(f)
 
-    return dataset['beam_force'], dataset['beam_length']
+    return dataset
 
 
 def main():
-    init_moment()
-    b_force, b_length = load_moment()
-    print(b_force[1]['Story'])
-    print(b_length['Length'][0])
+    init_pkl()
+    dataset = load_beam_design()
+    print(dataset[['Story', 'BayID']][682])
 
 
 if __name__ == '__main__':
