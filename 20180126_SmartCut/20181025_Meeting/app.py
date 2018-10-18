ab42676a0c4db64b172cf37ce5f205b320df40d5
skyran1278 <skyran1278@gmail.com>
2018-10-19 01:09:43 +0800

    meeting v

--- 20180126_SmartCut/20181025_Meeting/app.py
+++ 20180126_SmartCut/20181025_Meeting/app.py
@@ -7,20 +7,49 @@ import matplotlib.pyplot as plt
 from dataset_beam_design import load_beam_design
 from dataset_e2k import load_e2k
 
-rebars, stories, point_coordinates, lines, materials, sections = load_e2k()
 
-beam_design_table = load_beam_design()
-beam_design_table_immutable = load_beam_design()
-
-rebar_size = ['#4', '2#4', '2#5']
+rebar = ['#4', '2#4', '2#5', '2#6']
 max_spacing = 0.25
 min_spacing = 0.10
 
-a = beam_design.groupby(['Story', 'BayID'])
+rebars, stories, point_coordinates, lines, materials, sections = load_e2k()
 
-while expression:
-    pass
+beam_design_table = load_beam_design()
 
+# first time calc
+beam_design_table = beam_design_table.assign(dbt=rebar[0],
+                                             spacing=lambda x: rebars[rebar[0], 'AREA'] * 2 / x.VRebar)
+
+print(beam_design_table.head())
+
+for name, group in beam_design_table.groupby(['Story', 'BayID']):
+    i = 1
+    while np.any(group['spacing'] < min_spacing):
+        rebar_num, rebar_size = rebar[i].split(sep='#')
+        rebar_size = '#' + rebar_size
+
+        if rebar_num == '2':
+            def f(x): return rebars[rebar_size, 'AREA'] * 4 / x.VRebar
+        else:
+            def f(x): return rebars[rebar_size, 'AREA'] * 2 / x.VRebar
+
+        group = group.assign(dbt=rebar[i], spacing=f)
+
+        i += 1
+
+
+beam_design_table = beam_design_table.assign(
+    spacing=lambda x: np.minimum(x.spacing, max_spacing))
+
+# 支承寬要怎麼走
+for name, group in beam_design_table.groupby(['Story', 'BayID']):
+    np.amax(group['StnLoc'])
+
+    group['StnLoc'] <= np.amax(group['StnLoc']) - np.amin(group['StnLoc'])
+# while group['spacing']:
+#     pass
+
+# beam_design_table.groupby()
 # spacing = [beam_design[['Story', 'BayID', 'VRebar']],
 #            rebars[rebar_size, 'AREA'] * 2 / beam_design['VRebar']]
 
