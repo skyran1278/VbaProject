1fdf1bb4be9e83416562a56d922e5da4b5947a11
skyran1278 <skyran1278@gmail.com>
2018-12-04 10:14:41 +0800

    更新鋼筋資料庫

--- 20180126_SmartCut/LinearCut/dataset/const.py
+++ 20180126_SmartCut/LinearCut/dataset/const.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 E2K = '2018-1023_KaoHsiung -MRF_420 rebars'
-BEAM_DESIGN = '2018-1023_KaoHsiung -MRF_420 simple'
+BEAM_DESIGN = 'Concrete Design 2 - Beam Summary Data - ACI 318-05_IBC 2003'
 
 STIRRUP_REBAR = ['#4', '2#4', '2#5', '2#6']
 STIRRUP_SPACING = [10, 12, 15, 18, 20, 22, 25, 30]
