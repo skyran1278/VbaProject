da05e63d29e902a918150e88091bf5e321373003
skyran1278 <skyran1278@gmail.com>
2018-12-04 10:54:58 +0800

    重整架構

--- 20180126_SmartCut/VBA/VERSION.vb
+++ 20180126_SmartCut/VBA/VERSION.vb
@@ -10,8 +10,8 @@
 ' 隨工作簿不同而需更改的參數:
 ' VERSION_URL: 該工作簿 version.txt
 ' DOWNLOAD_URL: 該工作簿 下載檔案位置
-Private Const DOWNLOAD_URL = "https://github.com/skyran1278/VbaProject/raw/master/20180126_SmartCut/SmartCut.xlsm"
-Private Const VERSION_URL = "https://github.com/skyran1278/VbaProject/raw/master/20180126_SmartCut/Version.txt"
+Private Const DOWNLOAD_URL = "https://github.com/skyran1278/VbaProject/raw/master/20180126_SmartCut/VBA/SmartCut.xlsm"
+Private Const VERSION_URL = "https://github.com/skyran1278/VbaProject/raw/master/20180126_SmartCut/VBA/Version.txt"
 
 Sub VerifyPassword()
 '
