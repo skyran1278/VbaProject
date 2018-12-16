daad03ec0171fddc6c2860cc362cf7ae55823035
skyran1278 <skyran1278@gmail.com>
2018-12-16 18:56:45 +0800

    同步數據

--- 20180126_SmartCut/LinearCut/database/const.py
+++ 20180126_SmartCut/LinearCut/database/const.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-BEAM_DESIGN = '2018-0214 H2017-03D 欣詮建設中和福祥段14FB3 V2 test.xlsx'
-E2K = '2018-0214 H2017-03D 欣詮建設中和福祥段14FB3 V2.e2k'
+BEAM_DESIGN = '20181215 nonlinear v3 hinge DLLL.xlsx'
+E2K = '20181215 nonlinear v3 hinge DLLL.e2k'
 
 STIRRUP_REBAR = ['#4', '2#4', '2#5', '2#6']
 STIRRUP_SPACING = [10, 12, 15, 18, 20, 22, 25, 30]
@@ -14,6 +14,6 @@ BAR = {
 DB_SPACING = 1.5
 
 ITERATION_GAP = {
-    'Left': np.array([0.1, 0.45]),
-    'Right': np.array([0.55, 0.9])
+    'Left': np.array([0.2, 0.4]),
+    'Right': np.array([0.6, 0.8])
 }
