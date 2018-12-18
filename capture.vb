e54548da2c033b5ce7d2e40167d3b002eb2e520f
skyran1278 <skyran1278@gmail.com>
2018-12-19 01:18:49 +0800

    capture

--- capture.vb
+++ capture.vb
@@ -1,12 +1,9 @@
 Sub main()
 '
-' main 巨集
 '
-' 快速鍵: Ctrl+r
-'
-     position = Array("A3:AG35", "AK3:BQ35", "BU3:DA35", "DE3:EK35", "EO3:FU35", "A43:AG75", "AK43:BQ75", "BU43:DA75", "DE43:EK75", "EO43:FU75", "A83:AG115", "AK83:BQ115", "BU83:DA115", "DE83:EK115", "EO83:FU115", "A123:AG155", "AK123:BQ155", "BU123:DA155", "DE123:EK155", "EO123:FU155", "A163:AG195", "AK163:BQ195", "BU163:DA195", "DE163:EK195", "EO163:FU195", "A203:AG235", "AK203:BQ235", "BU203:DA235", "DE203:EK235", "EO203:FU235", "A243:AG275", "AK243:BQ275", "BU243:DA275", "DE243:EK275", "EO243:FU275", "A283:AG315", "AK283:BQ315", "BU283:DA315", "DE283:EK315", "EO283:FU315", "A323:AG355", "AK323:BQ355", "BU323:DA355", "DE323:EK355", "EO323:FU355", "A363:AG395", "AK363:BQ395", "BU363:DA395", "DE363:EK395", "EO363:FU395")
+    position = Array("A3:AG35", "AK3:BQ35", "BU3:DA35", "DE3:EK35", "EO3:FU35", "A43:AG75", "AK43:BQ75", "BU43:DA75", "DE43:EK75", "EO43:FU75", "A83:AG115", "AK83:BQ115", "BU83:DA115", "DE83:EK115", "EO83:FU115", "A123:AG155", "AK123:BQ155", "BU123:DA155", "DE123:EK155", "EO123:FU155", "A163:AG195", "AK163:BQ195", "BU163:DA195", "DE163:EK195", "EO163:FU195", "A203:AG235", "AK203:BQ235", "BU203:DA235", "DE203:EK235", "EO203:FU235", "A243:AG275", "AK243:BQ275", "BU243:DA275", "DE243:EK275", "EO243:FU275", "A283:AG315", "AK283:BQ315", "BU283:DA315", "DE283:EK315", "EO283:FU315", "A323:AG355", "AK323:BQ355", "BU323:DA355", "DE323:EK355", "EO323:FU355", "A363:AG395", "AK363:BQ395", "BU363:DA395", "DE363:EK395", "EO363:FU395")
 
-    For worksheets_index = 1 To 12 Step 1
+    For worksheets_index = 5 To 10 Step 5
         For position_index = LBound(position) To UBound(position) Step 1
             Call capture(worksheets_index, position_index, position(position_index))
         Next position_index
@@ -16,7 +13,6 @@ End Sub
 
 Sub capture(worksheets_index, position_index, position)
 '
-' capture 巨集
 '
 
     Sheets(worksheets_index).Activate
@@ -27,61 +23,16 @@ Sub capture(worksheets_index, position_index, position)
 
     Name = Split(ThisWorkbook.Name, ".xlsm")(0)
 
-    With ActiveSheet.ChartObjects.Add(0, 0, Rng.Width, Rng.Height)
+    Set co = Sheets(worksheets_index).ChartObjects.Add(0, 0, Rng.Width, Rng.Height)
+
+    co.Activate
+
+    With co
         .Chart.Paste
         .Chart.Export ThisWorkbook.Path & "\\" & Name & "-" & ActiveSheet.Name & "-" & position_index & ".JPEG"
         .Delete
     End With
 
-    Application.CutCopyMode=false
+    Application.CutCopyMode = False
 
 End Sub
-
-
-' Sub main()
-' '
-' ' main 巨集
-' '
-' ' 快速鍵: Ctrl+r
-' '
-'      position = Array("A3:AG35", "AK3:BQ35", "BU3:DA35", "DE3:EK35", "EO3:FU35", "A43:AG75", "AK43:BQ75", "BU43:DA75", "DE43:EK75", "EO43:FU75", "A83:AG115", "AK83:BQ115", "BU83:DA115", "DE83:EK115", "EO83:FU115", "A123:AG155", "AK123:BQ155", "BU123:DA155", "DE123:EK155", "EO123:FU155", "A163:AG195", "AK163:BQ195", "BU163:DA195", "DE163:EK195", "EO163:FU195", "A203:AG235", "AK203:BQ235", "BU203:DA235", "DE203:EK235", "EO203:FU235", "A243:AG275", "AK243:BQ275", "BU243:DA275", "DE243:EK275", "EO243:FU275", "A283:AG315", "AK283:BQ315", "BU283:DA315", "DE283:EK315", "EO283:FU315", "A323:AG355", "AK323:BQ355", "BU323:DA355", "DE323:EK355", "EO323:FU355", "A363:AG395", "AK363:BQ395", "BU363:DA395", "DE363:EK395", "EO363:FU395")
-
-'     For worksheets_index = 1 To 12 Step 1
-'         For position_index = LBound(position) To UBound(position) Step 1
-'             Call capture(worksheets_index, position_index, position(position_index))
-'         Next position_index
-'     Next worksheets_index
-
-' End Sub
-
-' Sub capture(worksheets_index, position_index, position)
-' '
-' ' capture 巨集
-' '
-' ' 快速鍵: Ctrl+g
-' '
-
-'     Sheets(worksheets_index).Activate
-
-'     Range(position).Copy
-
-'     Set shp = ActiveSheet.Pictures.Paste
-
-'     shp.CopyPicture
-
-'     Name = Split(ThisWorkbook.Name, ".xlsm")(0)
-
-'     With ActiveSheet.ChartObjects.Add(0, 0, shp.Width, shp.Height).Chart
-'         .Parent.Select
-'         .Paste
-'         .Export ThisWorkbook.Path & "\\" & Name & "-" & ActiveSheet.Name & "-" & position_index & ".JPEG"
-'         .Parent.Delete
-'     End With
-
-'     Application.CutCopyMode = False
-
-'     shp.Delete
-
-' End Sub
-
-
