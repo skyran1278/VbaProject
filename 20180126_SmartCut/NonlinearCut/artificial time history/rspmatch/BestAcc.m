bd5a7f5a5496b8d30ad21cb6ecef24c6cad31950
skyran1278 <skyran1278@gmail.com>
2018-12-20 21:38:55 +0800

    delete exe

--- 20180126_SmartCut/NonlinearCut/artificial time history/rspmatch/BestAcc.m
+++
@@ -1,6 +0,0 @@
-function [abest,apeak,misfit,bestmisfitall]=BestAcc(amod,abest,t,m,tetha,dt,Tall,wall,targetall,bestmisfitall)
-
-%This routine saves the best acceleration that gives the smallest average misfit during the Outer-loop
-
-[u,t_peak,t_index,apeak,misfit]=IniResponse(amod,t,m,tetha,dt,Tall,wall,targetall);
-if bestmisfitall>=mean(abs(misfit)), bestmisfitall=mean(abs(misfit)); abest=amod; end;
--- 20180126_SmartCut/NonlinearCut/artificial time history/rspmatch/Zeropad.m
+++
@@ -1,8 +0,0 @@
-function [arec,t]=Zeropad(zeropad,arec,dt)
-
-%This routine perfoms the zero padding on the acceleration time series
-
-pad=zeropad/dt;
-apad(1:pad)=0; apad(pad+1:pad+length(arec))=arec; apad(length(apad)+1:length(apad)+pad)=0;
-t=(0:length(apad)-1).*dt; t=t';
-amod=apad'; abest=amod; arec=apad'; clear apad
