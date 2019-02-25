b474f877c86293986615deb5264fa420f6cad688
skyran1278 <skyran1278@gmail.com>
2019-02-25 11:39:24 +0800

    fema

--- 20180126_SmartCut/Meeting/20190226 Meeting/fema/c1_nsp.m
+++ 20180126_SmartCut/Meeting/20190226 Meeting/fema/c1_nsp.m
@@ -2,10 +2,10 @@ function output = c1_nsp(Te, T0, R, c1)
     if Te >= T0
         output = 1.0;
     else
-        output = (1.0 + (R - 1) * T0 / Te) / R
+        output = (1.0 + (R - 1) * T0 / Te) / R;
     end
 
     if output > c1
-        output = c1
+        output = c1;
     end
 end
