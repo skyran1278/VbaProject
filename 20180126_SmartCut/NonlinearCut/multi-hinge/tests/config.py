8327cfaad1c48b6f8cfa23febd2bbdac016149d8
skyran1278 <skyran1278@gmail.com>
2019-03-25 10:27:46 +0800

    multihinge test ignore warning

--- 20180126_SmartCut/NonlinearCut/multi-hinge/tests/config.py
+++ 20180126_SmartCut/NonlinearCut/multi-hinge/tests/config.py
@@ -4,6 +4,6 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 config = {
-    'e2k_path': SCRIPT_DIR + '/tests/20190103 v3.0 3floor v16.e2k',
-    'design_path': SCRIPT_DIR + '/tests/20190323 203316 SmartCut.xlsx'
+    'e2k_path': SCRIPT_DIR + '/20190103 v3.0 3floor v16.e2k',
+    'design_path': SCRIPT_DIR + '/20190323 203316 SmartCut.xlsx'
 }
