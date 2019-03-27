b2e2b691bbd763b4b083f1b6d55819fe896bbb52
skyran1278 <skyran1278@gmail.com>
2019-03-27 22:57:19 +0800

    test data config

--- 20180126_SmartCut/NonlinearCut/multi-hinge/tests/config.py
+++ 20180126_SmartCut/NonlinearCut/multi-hinge/tests/config.py
@@ -3,11 +3,15 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-config = {
+config = {  # pylint: disable=invalid-name
     # 'e2k_path': SCRIPT_DIR + '/20190103 v3.0 3floor v16.e2k',
     'e2k_path_test_v1': SCRIPT_DIR + '/20190103 v3.0 3floor v16.e2k',
     'e2k_path_test_v2': SCRIPT_DIR + '/20190103 v3.0 3floor v16.e2k',
+    'e2k_path_test_v3': SCRIPT_DIR + '/2018-0214 H2017-03D 欣詮建設中和福祥段14FB3 V2_V13.e2k',
+    'e2k_path_test_v4': SCRIPT_DIR + '/2018-1023_KaoHsiung -MRF_420 V3_V13.e2k',
     # 'design_path': SCRIPT_DIR + '/20190323 203316 SmartCut.xlsx',
-    'design_path_test_v1': SCRIPT_DIR + '/20190323 203316 SmartCut.xlsx',
-    'design_path_test_v2': SCRIPT_DIR + '/20190327 164355 SmartCut.xlsx',
+    'design_path_test_v1': SCRIPT_DIR + '/20190323 203316 SmartCut #7.xlsx',
+    'design_path_test_v2': SCRIPT_DIR + '/20190327 164355 SmartCut #8.xlsx',
+    'design_path_test_v3': SCRIPT_DIR + '/20190327 173536 SmartCut 欣銓福祥.xlsx',
+    'design_path_test_v4': SCRIPT_DIR + '/20190327 180501 SmartCut 高雄物流.xlsx',
 }
