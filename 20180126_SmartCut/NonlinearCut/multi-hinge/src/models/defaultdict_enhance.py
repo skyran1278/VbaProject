23bdfe1bf60a930208c30f2e16e76384392c2a5e
skyran1278 <skyran1278@gmail.com>
2019-04-02 13:52:02 +0800

    defaultdict refactor

--- 20180126_SmartCut/NonlinearCut/multi-hinge/src/models/defaultdict_enhance.py
+++ 20180126_SmartCut/NonlinearCut/multi-hinge/src/models/defaultdict_enhance.py
@@ -1,7 +1,19 @@
 """
 sections post get put delete
 """
+# import warnings
 from collections import defaultdict
+
+
+def nest_defaultdict():
+    """
+    nest defaultdict
+    because defaultdict have side effect
+        when get key will create key = {}
+        it is difficult to debug
+        so maybe it is right to go back to simple {}
+    """
+    return defaultdict(nest_defaultdict)
 
 
 class DefaultdictEnhance:
@@ -19,7 +31,7 @@ class DefaultdictEnhance:
     """
 
     def __init__(self):
-        self.__data = defaultdict(dict)
+        self.__data = nest_defaultdict()
 
     def post(self, key, value=None, copy_from=None):
         """
@@ -41,13 +53,37 @@ class DefaultdictEnhance:
         """
         get
         """
-        if key is None:
-            return self.__data
+        if key2 is not None:
+            if key not in self.__data:
+                return None
+            if key2 not in self.__data[key]:
+                return None
+            return self.__data[key][key2]
 
-        if key2 is None:
+        if key is not None:
+            if key not in self.__data:
+                return None
             return self.__data[key]
 
-        return self.__data[key][key2]
+        return self.__data
+
+        # if key is None:
+        #     return self.__data
+
+        # if key2 is None:
+        #     if key not in self.__data:
+        #         # warnings.warn(f'{key} is not in dict')
+        #         return None
+        #     return self.__data[key]
+
+        # if key not in self.__data:
+        #     warnings.warn(f'{key} is not in dict')
+
+        # if key2 not in self.__data[key]:
+        #     warnings.warn(f'{key, key2} is not in dict')
+
+        # # return self.__data[key].get(key2, {})
+        # return self.__data[key][key2]
 
     def delete(self, key):
         """
@@ -71,6 +107,9 @@ def main():
     sections.post('B60', {'FYH': 28000})
     sections.post('B60', value=data)
     sections.post('B601', copy_from='B60', value={'FY': 42000})
+    sections.post('B59', copy_from='B60', value={'FY': 42000})
+    sections.post('B1', value=(1, 2))
+    print(sections.get('B59', 'FY'))
     print(sections.get())
 
 
