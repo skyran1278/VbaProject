708d0bf2cd86accdb5a2038f46bcf0e9eea01a6f
skyran1278 <skyran1278@gmail.com>
2019-04-05 01:04:19 +0800

    post loadcase

--- 20180126_SmartCut/NonlinearCut/time history/loadcase.py
+++ 20180126_SmartCut/NonlinearCut/time history/loadcase.py
@@ -1,4 +1,72 @@
+import os
 
+# global
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+initial_condition = 'PUSHDLLL'
+
+direction = 'U1'
+
+# different by model
+period1 = 0.344
+period2 = 0.088
+
+time_historys = {
+    'RSN169_IMPVALL.H_H-DLT262': [1, 2, 3],
+    'RSN953_NORTHR_MUL009': [1, 2]
+}
+
+# different by function
+
+for time_history in time_historys:
+    with open(f'{SCRIPT_DIR}//{time_history}.AT2', encoding='big5') as f:
+        pass
+
+with open(SCRIPT_DIR + '/loadcase.e2k', mode='w', encoding='big5') as f:
+    for name in time_historys:
+        post_function(f, name)
+
+
+def post_function(f, name):
+    """
+    path folder same as time history
+    """
+    f.write(
+        f'FUNCTION "{name}"  FUNCTYPE "HISTORY"  '
+        f'{SCRIPT_DIR}//{name}.AT2"  '
+        f'DATATYPE "EQUAL"  DT 0.01\n'
+    )
+    f.write(
+        f'FUNCTION "{name}"  HEADERLINES 4  POINTSPERLINE 5  FORMAT "FREE"'
+    )
+
+
+def post_loadcase(f):
+    name = f'{function}-{factor}'
+
+    f.write(
+        f'LOADCASE "{name}"  TYPE  "Nonlinear Direct Integration History"  '
+        f'INITCOND  "{initial_condition}"  MODALCASE  "Modal"  MASSSOURCE  "Previous"\n'
+    )
+
+    f.write(
+        f'LOADCASE "{name}"  ACCEL  "{direction}"  '
+        f'FUNC  "{function}"  SF  {factor}\n'
+    )
+    f.write(
+        f'LOADCASE "{name}"  NUMBEROUTPUTSTEPS  {output_step} '
+        f'OUTPUTSTEPSIZE  {delta_t}\n'
+    )
+    f.write(
+        f'LOADCASE "{name}"  PRODAMPTYPE  "Period"  T1  {period1} DAMP1  0.05 '
+        f'T2  {period2} DAMP2  0.05\n'
+    )
+    f.write(f'LOADCASE "{name}"  MODALDAMPTYPE  "None"\n')
+    f.write(f'LOADCASE "{name}"  USEEVENTSTEPPING  "No"\n')
+
+
+#   FUNCTION "RSN169_IMPVALL.H_H-DLT262"  FUNCTYPE "HISTORY"  FILE "C:\Users\skyran\Downloads\PEERNGARecords_Unscaled\RSN169_IMPVALL.H_H-DLT262.AT2"  DATATYPE "EQUAL"  DT 0.01
+#   FUNCTION "RSN169_IMPVALL.H_H-DLT262"  HEADERLINES 4  POINTSPERLINE 5  FORMAT "FREE"
 
 #   FUNCTION "chichi_TCU052_max"  FUNCTYPE "HISTORY"  FILE "D:\GitHub\VbaProject\20180126_SmartCut\NonlinearCut\time history\chichi_TCU052_max.txt"  DATATYPE "T&V"
 #   FUNCTION "chichi_TCU052_max"  POINTSPERLINE 1  FORMAT "FREE"
