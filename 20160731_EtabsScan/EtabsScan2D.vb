d0f95350335d09adc70c3b71cf8b53e5eb193029
skyran1278 <skyran1278@gmail.com>
2019-04-16 11:40:36 +0800

    etabsscan for 2D

--- 20160731_EtabsScan/EtabsScan2D.vb
+++ 20160731_EtabsScan/EtabsScan2D.vb
@@ -1,7 +1,6 @@
 Sub FilterData()
 
     Set Assemble = Worksheets("Assembled Point Masses")
-    Set Material = Worksheets("Material List By Story")
     Set StoryShears = Worksheets("Story Shears")
     Set Scan = Worksheets("Scan")
     Set DL = Worksheets("DL")
@@ -10,7 +9,6 @@ Sub FilterData()
     Set Dynamic = Worksheets("動態地震力修正")
 
     AssembleLastrow = Sheets("Assembled Point Masses").UsedRange.Rows.Count '------------抓取最後一行
-    MaterialLastrow = Sheets("Material List By Story").UsedRange.Rows.Count '------------抓取最後一行
     StoryShearsLastrow = Sheets("Story Shears").UsedRange.Rows.Count '------------抓取最後一行
 
 ' ---------先刪除原有的表格，DEBUG
@@ -20,7 +18,6 @@ Sub FilterData()
     Dynamic.Cells.ClearContents
     For i = 3 To 17 Step 7
         Scan.Range(Cells(14, i), Cells(10000, i + 1)).ClearContents
-        Scan.Range(Cells(14, i + 3), Cells(10000, i + 3)).ClearContents
     Next
 
 '---------複製Story Shears貼上到各Sheet
@@ -45,10 +42,6 @@ Sub FilterData()
 
     EQfloor = Scan.Cells(2, 4)
 
-    Sheets("Material List By Story").Select
-    Cells.Select
-    Selection.AutoFilter
-    ActiveSheet.Range(Cells(1, 1), Cells(MaterialLastrow, 8)).AutoFilter Field:=2, Criteria1:="Floor"
     Sheets("DL").Select
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=2, Criteria1:="DL"
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=3, Criteria1:="Bottom"
@@ -57,12 +50,12 @@ Sub FilterData()
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=3, Criteria1:="Bottom"
     Sheets("靜態地震力").Select
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=2, Criteria1:=Array( _
-        "DL", "EQXN", "EQXP", "EQYN", "EQYP"), Operator:=xlFilterValues
+        "DL", "EQV", "EQX"), Operator:=xlFilterValues
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=3, Criteria1:="Bottom"
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=1, Criteria1:=EQfloor
     Sheets("動態地震力修正").Select
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=2, Criteria1:=Array( _
-        "0SPECX", "0SPECY", "EQV", "EQXN", "EQXP", "EQYN", "EQYP", "SPECXF MAX", "SPECYF MAX") _
+        "0SPECX", "EQX", "SPECXF MAX") _
         , Operator:=xlFilterValues
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=3, Criteria1:="Bottom"
     ActiveSheet.Range(Cells(1, 1), Cells(StoryShearsLastrow, 9)).AutoFilter Field:=1, Criteria1:=EQfloor
@@ -115,33 +108,6 @@ Sub FilterData()
     Selection.Copy
     Sheets("Scan").Select
     Range("K14").Select
-    'ActiveSheet.Paste
-    Selection.PasteSpecial Paste:=xlPasteValuesAndNumberFormats, Operation:= _
-        xlNone, SkipBlanks:=False, Transpose:=False
-
-    '------------------貼上FloorArea
-    Sheets("Material List By Story").Select
-    Range(Cells(4, 5), Cells(MaterialLastrow, 5)).Select
-    Selection.Copy
-    Sheets("Scan").Select
-    Range("F14").Select
-    'ActiveSheet.Paste
-    Selection.PasteSpecial Paste:=xlPasteValuesAndNumberFormats, Operation:= _
-        xlNone, SkipBlanks:=False, Transpose:=False
-
-    '-----------------修正FloorArea
-    FloorAreaLastrow = Scan.Cells(Scan.Rows.Count, "F").End(xlUp).Row
-    StoryLastrow = Scan.Cells(Scan.Rows.Count, "C").End(xlUp).Row
-    Cells(StoryLastrow, 6) = Cells(FloorAreaLastrow, 6)
-    Cells(FloorAreaLastrow, 6) = ""
-    Range(Cells(14, 6), Cells(StoryLastrow, 6)).Select
-    Selection.Copy
-    '-----------------重貼FloorArea
-    Range("M14").Select
-    'ActiveSheet.Paste
-    Selection.PasteSpecial Paste:=xlPasteValuesAndNumberFormats, Operation:= _
-        xlNone, SkipBlanks:=False, Transpose:=False
-    Range("T14").Select
     'ActiveSheet.Paste
     Selection.PasteSpecial Paste:=xlPasteValuesAndNumberFormats, Operation:= _
         xlNone, SkipBlanks:=False, Transpose:=False
