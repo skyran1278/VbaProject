f1a015905d0f2056fa3bb13fe9cb4dcb2f482822
skyran1278 <skyran1278@gmail.com>
2019-05-08 17:41:58 +0800

    design spectrum

--- 20180126_SmartCut/NonlinearCut/time history/spectrum/spectrum.m
+++ 20180126_SmartCut/NonlinearCut/time history/spectrum/spectrum.m
@@ -1,90 +1,76 @@
 clc; clear; close all;
 
-filename = 'RSN68_SFERN_PEL090';
+structure_period = 1.01;
+ss_s1 = [0.8, 0.675, 1, 0.77];
 
-[ag, time_interval, NPTS, errCode] = parseAT2('../PEERNGARecords_Unscaled/RSN68_SFERN_PEL090.AT2');
+% all(42) time history
+% filenames = [
+%   "RSN68_SFERN_PEL090", "RSN68_SFERN_PEL180", "RSN125_FRIULI.A_A-TMZ000", "RSN125_FRIULI.A_A-TMZ270", "RSN169_IMPVALL.H_H-DLT262", "RSN169_IMPVALL.H_H-DLT352", "RSN174_IMPVALL.H_H-E11140", "RSN174_IMPVALL.H_H-E11230", "RSN721_SUPER.B_B-ICC000", "RSN721_SUPER.B_B-ICC090", "RSN725_SUPER.B_B-POE270", "RSN725_SUPER.B_B-POE360", "RSN752_LOMAP_CAP000", "RSN752_LOMAP_CAP090", "RSN767_LOMAP_G03000", "RSN767_LOMAP_G03090", "RSN848_LANDERS_CLW-LN", "RSN848_LANDERS_CLW-TR", "RSN900_LANDERS_YER270", "RSN900_LANDERS_YER360", "RSN953_NORTHR_MUL009", "RSN953_NORTHR_MUL279", "RSN960_NORTHR_LOS000", "RSN960_NORTHR_LOS270", "RSN1111_KOBE_NIS000", "RSN1111_KOBE_NIS090", "RSN1116_KOBE_SHI000", "RSN1116_KOBE_SHI090", "RSN1148_KOCAELI_ARE000", "RSN1148_KOCAELI_ARE090", "RSN1158_KOCAELI_DZC180", "RSN1158_KOCAELI_DZC270", "RSN1244_CHICHI_CHY101-E", "RSN1244_CHICHI_CHY101-N", "RSN1485_CHICHI_TCU045-E", "RSN1485_CHICHI_TCU045-N", "RSN1602_DUZCE_BOL000", "RSN1602_DUZCE_BOL090", "RSN1633_MANJIL_ABBAR--L", "RSN1633_MANJIL_ABBAR--T", "RSN1787_HECTOR_HEC000", "RSN1787_HECTOR_HEC090"
+% ];
 
-period = 0 : time_interval : (NPTS - 1) * time_interval;
-% ag = Acc;
+% NM = [
+%   1.910, 2.449, 1.815, 1.359, 1.576, 1.257, 1.152, 0.930, 0.863, 0.993, 1.008, 1.430, 1.091, 1.401, 1.143, 0.913, 1.502, 0.955, 0.811, 1.426, 0.700, 0.622, 0.935, 1.009, 0.886, 1.084, 1.324, 1.902, 2.973, 1.035, 0.705, 0.745, 0.638, 0.380, 0.828, 0.894, 0.742, 0.630, 0.977, 0.820, 1.595, 0.926
+% ];
 
-tn = 0.001 : 0.01 : 1;
+% % 21 time history
+filenames = [
+  "RSN68_SFERN_PEL090", "RSN125_FRIULI.A_A-TMZ270", "RSN169_IMPVALL.H_H-DLT262", "RSN174_IMPVALL.H_H-E11230", "RSN721_SUPER.B_B-ICC090", "RSN725_SUPER.B_B-POE360", "RSN752_LOMAP_CAP000", "RSN767_LOMAP_G03090", "RSN848_LANDERS_CLW-TR", "RSN900_LANDERS_YER270", "RSN953_NORTHR_MUL279", "RSN960_NORTHR_LOS000", "RSN1111_KOBE_NIS000", "RSN1116_KOBE_SHI000", "RSN1148_KOCAELI_ARE090", "RSN1158_KOCAELI_DZC180", "RSN1244_CHICHI_CHY101-N", "RSN1485_CHICHI_TCU045-E", "RSN1602_DUZCE_BOL090", "RSN1633_MANJIL_ABBAR--T", "RSN1787_HECTOR_HEC090"
+];
+
+NM = [
+  2.054, 1.462, 1.695, 1.000, 1.067, 1.537, 1.173, 0.982, 1.027, 0.873, 0.669, 1.005, 0.953, 1.424, 1.113, 0.758, 0.408, 0.891, 0.677, 0.882, 0.996
+];
+
+% pick 10 time history
+% filenames = [
+%   "RSN125_FRIULI.A_A-TMZ000", "RSN767_LOMAP_G03000", "RSN1148_KOCAELI_ARE000", "RSN1602_DUZCE_BOL000", "RSN1111_KOBE_NIS090", "RSN1633_MANJIL_ABBAR--L", "RSN725_SUPER.B_B-POE270", "RSN68_SFERN_PEL180", "RSN960_NORTHR_LOS270", "RSN1485_CHICHI_TCU045-N"
+% ];
+
+% NM = [
+%   1.737, 1.093, 2.845, 0.710, 1.037, 0.935, 0.964, 2.343, 0.965, 0.856
+% ];
+
+tn = 0.001 : 0.01 : 5;
 tn_length = length(tn);
-acceleration = zeros(1, tn_length);
+acceleration = zeros(length(filenames), tn_length);
 
-% time_interval = record_dt;
+for filename = 1 : length(filenames)
+    [ag, time_interval, NPTS, errCode] = parseAT2('../PEERNGARecords_Unscaled/' + filenames(filename) + '.AT2');
 
-for index = 1 : tn_length
+    period = 0 : time_interval : (NPTS - 1) * time_interval;
 
-    [~, ~, a_array] = newmark_beta(ag, time_interval, 0.05, tn(index), 'average');
+    for index = 1 : tn_length
 
-    acceleration(1, index) = max(abs(a_array));
+        [~, ~, a_array] = newmark_beta(ag, time_interval, 0.05, tn(index), 'average');
+
+        acceleration(filename, index) = max(abs(a_array));
+
+    end
+
+    acceleration(filename, :) = acceleration(filename, :) * NM(filename);
 
 end
 
-% tol = eps(0.5);
+median_acceleration = median(acceleration);
 
-fprintf('Records: %s, PGA: %.3f, PGA: %.3f\n', max(abs(ag)), acceleration(1));
-
-figure;
-plot(tn, acceleration);
-title(filename);
-xlabel('T(sec)');
-ylabel('Sa(g)');
+% 0.5 0.3 0.7 0.4
+[sad, ~] = design_spectrum(ss_s1(1), ss_s1(2), 0.05, tn);
+[sam, ~] = design_spectrum(ss_s1(3), ss_s1(4), 0.05, tn);
 
 figure;
-plot(period, ag);
-title(filename);
 xlabel('T(sec)');
 ylabel('Sa(g)');
+hold on;
 
-% figure;
-% hold on;
-% title(filename);
-% xlabel('T(sec)');
-% ylabel('Sa(g)');
+plot(tn, sad);
+plot(tn, sam);
+plot(tn, median_acceleration);
 
-% for intensity = 0.5 : 0.5 : 3
+tol = 0.001;
 
-%     for index = 1 : tn_length
+period_index = abs(tn - structure_period) < tol;
 
-%         scaled_ag = intensity * ag;
+sa = median_acceleration(period_index);
 
-%         [~, ~, a_array] = newmark_beta(scaled_ag, time_interval, 0.05, tn(index), 'average');
-
-%         acceleration(1, index) = max(abs(a_array));
-
-%     end
-
-%     fprintf('Intensity: %.1f, PGA: %.3f, PGA: %.3f, Sa: %.3f, PGA Scaled: %.3f, Sa Scaled: %.3f\n', intensity, max(scaled_ag), acceleration(1), acceleration(tn == 0.295), max(scaled_ag) / intensity, acceleration(tn == 0.295) / intensity);
-
-%     plot(tn, acceleration);
-
-% end
-
-
-
-% fileID = fopen('ELC.txt', 'w');
-% fprintf(fileID, '%f \r\n', ag);
-% fclose(fileID);
-% clc; clear; close all;
-
-% ag = filename_to_array('I-ELC270_gal_l00Hz', 2, 2);
-
-% tn = 0.01 : 0.01 : 5;
-% tn_length = length(tn);
-% acceleration = zeros(1, tn_length);
-
-% for index = 1 : tn_length
-
-%     [~, ~, a_array] = newmark_beta(ag, 0.01, 0.05, tn(index), 'average');
-
-%     acceleration(1, index) = max(abs(a_array));
-
-% end
-
-% acceleration_normal = acceleration / acceleration(1, 1) * 0.4;
-% acceleration_normal(tn == 2.5)
-% figure;
-% plot(tn, acceleration_normal);
-% xlabel('T(sec)');
-% ylabel('SaD(g)');
+fprintf('Sa: %.3f, SaD Factor: %.3f, SaM Factor: %.3f\n', sa, sad(period_index) / sa, sam(period_index) / sa);
+% fprintf('Records: %s, PGA: %.3f, PGA: %.3f\n', max(abs(ag)), acceleration(1));
