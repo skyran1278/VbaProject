0bbe634e91dd201ba51cce8ee1931d2ef866d4ac
skyran1278 <skyran1278@gmail.com>
2019-11-25 09:51:31 +0800

    remove smart cut

--- 20180126_SmartCut/Meeting/20190315 Meeting/Time History/convert_unit.m
+++
@@ -1,7 +0,0 @@
-function value = convert_unit(unit)
-    if unit == 'gal2m'
-        value = 0.01
-    elseif unit == 'gal2m'
-        value = 0.01
-    end
-end
--- 20180126_SmartCut/NonlinearCut/time history/artificial time history/README.md
+++
@@ -1,1 +0,0 @@
-https://www.evernote.com/l/AajGn04zTP5H8ap1Tc_E7P22HBAWdrrCQ-U/
